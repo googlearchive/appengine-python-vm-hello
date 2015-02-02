162449df9612d431e3cb6163c924f3c2ae3db71c
elibixby <elibixby@localhost>
2015-02-01 17:36:12 -0800

    Added code in master 'node'

--- flow.groovy
+++ flow.groovy
@@ -1,4 +1,13 @@
-node {
+
+node('master'){
+	def slurper = new groovy.json.JsonSlurper()
+	def pod = slurper.parse(new File('pod_template.json'))
+	pod.setId(env.BUILD_TAG)
+	
+	kube_create_pod: pod
+}
+
+node(env.BUILD_TAG) {
 	sh "gcloud config set project civil-authority-768"
 	sh "gcloud auth login"
 	sh "gcloud preview app deploy ."
