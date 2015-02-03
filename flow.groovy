42ac2bd66559029a3b5859009cdca8908b47ae83
elibixby <eli.bixby@gmail.com>
2015-02-03 10:27:50 -0800

    again

--- flow.groovy
+++ flow.groovy
@@ -3,7 +3,7 @@ node('master'){
 	//Vodoo string manipulation:
 	//replaces environmental variables defined in pod_template.json with their binding
 	//probably better to write a workflow tool for this
-	sh 'perl -p -e \'s/\\$\\{([^}]+)\\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\\$\\{([^}]+)\\}//eg\' $WORKSPACE/build/pod_template.json | $JENKINS_HOME/kubectl create -f -'
+	sh 'perl -p -e \'s/\\$\\{([^}]+)\\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\\$\\{([^}]+)\\}//eg\' $WORKSPACE/pod_template.json | $JENKINS_HOME/kubectl create -f -'
 }
 
 node(env.BUILD_TAG) {
