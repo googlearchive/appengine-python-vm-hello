2670a477139bc7dd1010991c7d69d3da4d5898ff
elibixby <elibixby@localhost>
2015-02-03 16:03:24 -0800

    Try Get Step

--- flow.groovy
+++ flow.groovy
@@ -1,32 +1,33 @@
 
 node('master'){
-	step(kube_create("pods", [
-		"id": env.BUILD_TAG,
-		"kind": "Pod",
-		"apiVersion": "v1beta1",
-		"desiredState": [
-			"manifest": [
-				"version": "v1beta1",
-				"containers": [[
-					"name": "jenkinsslave",
-					"image": "elibixby/jenkins_slave",
-					"volumeMounts": [
-						"name": "docksock",
-						"mountpath": "/var/run"
-					]
-				]],
-				"volumes": [[
-					"name": "docksock",
-					"source": [
-						"hostDir": [
-							"path": "/var/run"
-						]
-					]
-				]]
-			]
-		]
-	]))
-}
+	kube_create("pods", "jenkinsmaster1")
+#[
+##		"id": env.BUILD_TAG,
+##		"kind": "Pod",
+##		"apiVersion": "v1beta1",
+##		"desiredState": [
+##			"manifest": [
+##				"version": "v1beta1",
+##				"containers": [[
+##					"name": "jenkinsslave",
+##					"image": "elibixby/jenkins_slave",
+##					"volumeMounts": [
+##						"name": "docksock",
+##						"mountpath": "/var/run"
+##					]
+##				]],
+##				"volumes": [[
+##					"name": "docksock",
+##					"source": [
+##						"hostDir": [
+##							"path": "/var/run"
+##						]
+##					]
+##				]]
+##			]
+##		]
+##	]))
+#}
 
 node(env.BUILD_TAG) {
 	sh "gcloud config set project civil-authority-768"
