29a3248a1b7da453ba521870736efc3dce8d3703
Bill Prin <waprin@google.com>
2015-06-29 18:32:02 -0700

    Print error on HTTP exception

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ from api_key import key
 def get_author(title):
     host = 'https://www.googleapis.com/books/v1/volumes?q={}&key={}&country=US'.format(title, key)
     request = urllib2.Request(host)
-    response = urllib2.urlopen(request)
+    try:
+        response = urllib2.urlopen(request)
+    except urrllib2.HTTPError, error:
+        contents = error.read()
+        return str(contents)
     html = response.read()
     author = json.loads(html)['items'][0]['volumeInfo']['authors'][0]
     return author
