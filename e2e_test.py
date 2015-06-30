84cdb8ed56fbb6b3f1468c6daff24772316d52b1
Bill Prin <waprin@google.com>
2015-06-29 18:27:13 -0700

    Undo typo fix

--- e2e_test.py
+++ e2e_test.py
@@ -12,7 +12,6 @@ import urllib2
 import logging
 
 HOST='http://continuous-deployment-python.appspot.com'
-HOST='http://continuous-deployment-python.appspot.com'
 
 response = urllib2.urlopen("{}/get_author/ulysses".format(HOST))
 html = response.read()
